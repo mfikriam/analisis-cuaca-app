e94a986fc7107ab57bb1eb3c6808480e75421085
mfikriam <yapkun17@gmail.com>
2024-01-13 15:17:43 +0800

    remove clg

--- controllers/authController.js
+++ controllers/authController.js
@@ -81,8 +81,6 @@ exports.protect = catchAsync(async (req, res, next) => {
   // ? 2) Verification token
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
-  console.log('decoded', decoded);
-
   // ? 3) Check if user still exists
   const currentUser = await User.findByPk(decoded.id);
   if (!currentUser) {
